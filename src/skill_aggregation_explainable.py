534fe363e3e32b3e9aa2956e5583c3c293e8dc0b
Nipuni Siriwardhana <157195014+nsiriwardhana@users.noreply.github.com>
2025-11-25 14:26:22 +0530

    Add files

---
+++ src/skill_aggregation_explainable.py
@@ -0,0 +1,7 @@
+import os
+from typing import Dict, List
+
+import pandas as pd
+
+from course_skill_mapping import load_course_skill_mapping
+from transcript_loader import load_transcripts_long  # make sure this function exists
