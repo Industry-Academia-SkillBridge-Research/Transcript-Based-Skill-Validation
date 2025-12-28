1ed670ff0c4e5015be966b31a70db750d1a89184
Nipuni Siriwardhana <157195014+nsiriwardhana@users.noreply.github.com>
2025-12-28 08:05:52 +0530

    add

--- src/api/main.py
+++ src/api/main.py
@@ -32,7 +32,10 @@ app = FastAPI(
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "http://127.0.0.1:5173",   # Vite default
+    "http://localhost:5173",
 ]
+
 
 app.add_middleware(
     CORSMiddleware,
